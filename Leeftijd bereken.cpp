7c6cb69045d14b46536fb24df98fc18161395604
Khedoe-Rishaan <rishaankhedoe@gmail.com>
2025-01-20 12:02:51 -0800

    Add files via upload

---
+++ Leeftijd bereken.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int geboorteDatum ,huidigeJaar,age;
+    huidigeJaar=2024;
+     cout<<"Type your born date:";
+    cin>>geboorteDatum;
+    age=huidigeJaar-geboorteDatum;
+    cout<<"Your age is :"<<age<<endl;
+    return 0 ;
+}
