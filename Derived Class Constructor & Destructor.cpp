7111211102abd01a21d7de0c162adbbd97b7c105
Khedoe-Rishaan <rishaankhedoe@gmail.com>
2025-09-06 11:43:34 -0300

    Update Derived Class Constructor & Destructor.cpp

--- Derived Class Constructor & Destructor.cpp
+++ Derived Class Constructor & Destructor.cpp
@@ -20,7 +20,10 @@ class Daughter:public Mother{
 };
 int main(){
     Daughter d;
-    
-    
-
 }
+
+/*Bij het erven van klassen worden de constructor en destructor van de basisklasse niet geërfd.
+Ze worden echter wel aangeroepen wanneer een object van de afgeleide klasse wordt gemaakt of verwijderd.
+Om dit gedrag verder uit te leggen, maken we een voorbeeldklasse met een constructor en een destructor.
+De constructor van de basisklasse wordt als eerste aangeroepen.
+De destructor van de afgeleide klasse wordt eerst aangeroepen en daarna de destructor van de basisklasse.*/
