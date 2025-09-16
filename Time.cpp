d1c552bee18e237e7078866cdd902951e2cc2a10
Khedoe-Rishaan <rishaankhedoe@gmail.com>
2025-09-16 13:31:14 -0300

    Create Time.cpp

---
+++ Time.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+class student {
+    private:
+    int studentnummer;
+    int admission_year;
+    int age;
+    string name;
+    
+    public:
+    student(int st,int ad, int ag, string na):studentnummer(st),admission_year(ad),age(ag),name(na){ };
+    void details(){
+        cout<<"studentnummer:"<<studentnummer<<endl;
+        cout<<"admission_year:"<<admission_year<<endl;
+        cout<<"age:"<<age<<endl;
+        cout<<"name:"<<name<<endl;
+    }
+};
+int main(){
+    student stu(12345, 2023, 20, "Jan");
+    stu.details();
+    return 0 ;
+}
+*/
+
+
+/*Schrijf een klasse Tijdstip met daarin drie private leden van het type int voor de uren, de minuten en de seconden, een constructor met default-argumenten en een public lidfunctie to_string() die de tijd aflevert in het formaat uu:mm:ss, dus bijvoorbeeld 15:05:03 of 14:00:59. Test de klasse in een programma.*/
+
+class Tijdstip{
+    private:
+    int uren;
+    int minuten;
+    int seconden;
+    public:
+    Tijdstip(int ur,int mi,int se):uren(ur),minuten(mi),seconden(se){};
+    void to_string(){
+        cout<<uren<<":"<<minuten<<":"<<seconden<<endl;
+    }
+    
+    
+};
+int main(){
+    Tijdstip tijd1(15,05,03);
+    tijd1.to_string();
+}
